ec57b3cf262d602a03f09ba30c3f6cc584840369
André Breia <andre.breia.22@gmail.com>
2020-01-23 19:17:42 +0000

    Remove analytics from code

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -6,8 +6,6 @@
 
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
-    @include('_layouts.partials.analytics')
 </head>
 
 <body class="flex flex-col min-h-screen font-sans text-blue-grey-lighter bg-gradient-primary leading-normal text-lg pt-32 border-t-8 border-brand-secondary">
