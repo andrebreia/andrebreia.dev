a13c98f904dd8483bb7305a09acd98f90c22faf6
André Breia <andre.breia.22@gmail.com>
2022-11-12 12:25:08 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,17 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // The various configurable Tailwind configuration files.
-  presets: [
-    require('tailwindcss/defaultConfig'),
-    require('./tailwind.config.typography.js'),
-    require('./tailwind.config.peak.js'),
-    require('./tailwind.config.site.js'),
-  ],
-  // Uncomment the next line to enable class based dark mode: https://peak.1902.studio/features/dark-mode.html.
-  // darkMode: 'class',
-  mode: 'jit',
-  // Configure Purge CSS.
-  content: [
-    './resources/views/**/*.html',
-    './resources/js/**/*.js',
-  ],
-  safelist: []
-}
+    // The various configurable Tailwind configuration files.
+    presets: [
+        require("tailwindcss/defaultConfig"),
+        require("./tailwind.config.typography.js"),
+        require("./tailwind.config.peak.js"),
+        require("./tailwind.config.site.js"),
+    ],
+    // Uncomment the next line to enable class based dark mode: https://peak.1902.studio/features/dark-mode.html.
+    darkMode: "class",
+    mode: "jit",
+    // Configure Purge CSS.
+    content: ["./resources/views/**/*.html", "./resources/js/**/*.js"],
+    safelist: [],
+};
